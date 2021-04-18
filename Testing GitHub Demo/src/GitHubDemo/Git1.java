d0e44249316b2fb3071f97c02cf4780370632da6
user <user@LAPTOP-QOF44QE3>
2021-04-19 02:16:46 +0530

    by abc

--- Testing GitHub Demo/src/GitHubDemo/Git1.java
+++ Testing GitHub Demo/src/GitHubDemo/Git1.java
@@ -1,7 +1,11 @@
 package GitHubDemo;
 
 public class Git1 {
-public static void main(String[] args) {
-	System.out.println("Hello");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	public void add()
+	{
+		System.out.println("add method1");	
+	}
 }
